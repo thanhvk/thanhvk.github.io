fc2ff8f0c96ac6d227f0ee737a56ed6289c479af
thanhvk <thanhvk.w3s@gmail.com>
2016-05-30 00:32:32 +0700

    Add couch surfing template

---
+++ cs/js/app.js
@@ -0,0 +1,70 @@
+var main = function(){
+
+	//Slide images when start
+
+	var slide = setInterval(function(){nextSlide()}, 9000);
+	
+	//Build function next slider
+	
+	var nextSlide = function(){
+		var currentSlide = $('.active-slide');
+		var nextSlide = currentSlide.next();
+		
+		if(nextSlide.length === 0){
+			nextSlide = $('.slide').first();
+		};
+		
+		currentSlide.fadeOut(3000).removeClass('active-slide');
+		nextSlide.fadeIn(3000).addClass('active-slide');
+		
+		var currentDot = $('.active-dot');
+		var nextDot = currentDot.next();
+		
+		if (nextDot.length === 0){
+			nextDot = $('.dot').first();
+		};
+		
+		currentDot.removeClass('active-dot');
+		nextDot.addClass('active-dot');
+		};
+	
+	//Even click next button
+		
+	$('.narrow-next').click(function(){
+		clearInterval(slide);
+		nextSlide();
+	});
+	
+	// Build function previous slider
+	
+	var prevSlide = function(){
+		var currentSlide = $('.active-slide');
+		var prevSlide = currentSlide.prev();
+		
+		if(prevSlide.length === 0){
+			prevSlide = $('.slide').last();
+		};
+		
+		currentSlide.fadeOut(3000).removeClass('active-slide');
+		prevSlide.fadeIn(3000).addClass('active-slide');
+		
+		var currentDot = $('.active-dot');
+		var prevDot = currentDot.prev();
+		
+		if (prevDot.length === 0){
+			prevDot = $('.dot').last();
+		};
+		
+		currentDot.removeClass('active-dot');
+		prevDot.addClass('active-dot');
+	};
+	
+	//Even click previous button
+	
+	$('.narrow-prev').click(function(){
+		clearInterval(slide);
+		prevSlide();
+	});
+};
+
+$(document).ready(main);
---
+++ cs/readme.txt
@@ -0,0 +1,19 @@
+For Desktop 
+Not responsive and perfect, but my's first product. After, I teach yourself HTML&CSS, JQuery, Javascript on codecademy.com and I love codecademy.com and it too. 
+
+index.html
+|
+css--bootstrap-responsive.min.css
+   --bootstrap.min.css
+   --style.css
+|
+js--html5shiv.js
+  --jquery-1.11.1.min.js
+  --app.js
+|
+images
+|
+psd--Mockup.psd
+
+email: thanhvk.w3s@gmail.com
+phone: 0988.439.328
