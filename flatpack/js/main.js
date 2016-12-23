b87b1d30a3216164e349f1334b8bd2ed0ef27d2d
thanhvk <thanhvk.w3s@gmail.com>
2016-12-24 03:56:07 +0700

    Add scroll anchor

--- flatpack/js/main.js
+++ flatpack/js/main.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
 	$.scrollSpeed(120, 800);
+
+	$('a').smoothScroll({speed: 800});
+
+	$('.hamburger').click(function() {
+		var navbar 		= $('.navbar-collapse'),
+			collapse 	= navbar.hasClass('collapse');
+
+		if (collapse) {
+			navbar.removeClass('collapse');
+		} else {
+			navbar.addClass('collapse');
+		}
+	});
 });
